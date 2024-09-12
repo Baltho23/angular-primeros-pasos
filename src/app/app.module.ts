ee55a12b85fb80c8765aaa331e9cd9b5c5f7d758
Airton Sampayo <aldairton23@gmail.com>
2024-09-12 17:28:02 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    DbzModule,
+    HeroesModule,
+    CounterModule,
     BrowserModule,
     AppRoutingModule
   ],
